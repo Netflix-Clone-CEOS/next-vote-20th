e1abd6ced0be764fd92633062f6ef90d691fa68a
HAE2NI <hae0_02ni@ewhain.net>
2024-12-26 11:05:49 +0900

    design: demovotepage

--- vote/src/app/demovote/page.tsx
+++ vote/src/app/demovote/page.tsx
@@ -1,5 +1,15 @@
+import Buttons from "@/DemoDay/components/voting/Buttons";
+import Container from "@/DemoDay/components/voting/Container";
 import React from "react";
 
 export default function page() {
-  return <div>page</div>;
+  return (
+    <div>
+      <p>데모데이 투표</p>
+      <Container />
+      <div>
+        <Buttons />
+      </div>
+    </div>
+  );
 }
