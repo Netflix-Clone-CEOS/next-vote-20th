825bf32cda9d1d96ca853b0073bb8b22506561c8
HAE2NI <hae0_02ni@ewhain.net>
2024-12-26 11:58:42 +0900

    design: buttons from header

--- vote/src/components/Buttons.tsx
+++ vote/src/components/Buttons.tsx
@@ -1,10 +1,11 @@
 import Button from "./Button";
+import { buttons } from "@/styles/headerBtn.css";
 
 const BUTTON_LIST = ["로그인", "회원가입"];
 
 export default function Buttons() {
   return (
-    <div>
+    <div className={buttons}>
       {BUTTON_LIST.map((text) =>
         text === "로그인" ? (
           <Button variant="login" key={text} text={text} />
