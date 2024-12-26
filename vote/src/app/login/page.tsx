29c275d5924b6e4ab0f19ecfc32a2254332dda1a
HAE2NI <hae0_02ni@ewhain.net>
2024-12-26 10:50:22 +0900

    design: loginPage

--- vote/src/app/login/page.tsx
+++ vote/src/app/login/page.tsx
@@ -1,3 +1,10 @@
+import LoginInputs from "./components/LoginInputs";
+
 export default function page() {
-  return <div>page</div>;
+  return (
+    <div>
+      <p>로그인</p>
+      <LoginInputs />
+    </div>
+  );
 }
