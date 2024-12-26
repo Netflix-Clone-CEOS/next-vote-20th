662b772e4e1fc55396c0ea3b1b11470f624de442
HAE2NI <hae0_02ni@ewhain.net>
2024-12-26 20:30:52 +0900

    desgin: header

--- vote/src/components/Header.tsx
+++ vote/src/components/Header.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 import Buttons from "./Buttons";
+import Ceos from "@/components/Ceos";
+import { headerContainer } from "./styles/header.css";
 
 export default function Header() {
   return (
-    <header>
+    <header className={headerContainer}>
+      <Ceos />
       <Buttons />
     </header>
   );
---
+++ vote/src/components/styles/header.css.ts
@@ -0,0 +1,9 @@
+import { style } from "@vanilla-extract/css";
+
+export const headerContainer = style({
+  display: "flex",
+  justifyContent: "space-between",
+  alignItems: "center",
+
+  width: "135rem",
+});
