100264ad9129d744e7c8c63c1b4f3e4a5ca6f62f
Gabin Lee <gb94@ewhain.net>
2025-01-04 15:05:16 +0900

    [ chore ] 헤더 로고 네비게이션 추가

--- vote/src/components/Header.tsx
+++ vote/src/components/Header.tsx
@@ -1,5 +1,6 @@
 "use client";
 import React from "react";
+import { useRouter } from 'next/navigation';
 import Buttons from "./Buttons";
 import Ceos from "@/components/Ceos";
 import { headerContainer, logined_container } from "./styles/header.css";
@@ -9,14 +10,22 @@ import LoginedHeader from "./LoginedHeader";
 import Logout from "./Logout";
 
 export default function Header() {
+  const router = useRouter();
+
   const { data } = useQuery({
     queryKey: ["member"],
     queryFn: member,
   });
 
+  const handleCeosClick = () => {
+    router.push("/");
+  };
+
   return (
     <header className={headerContainer}>
-      <Ceos />
+      <div onClick={handleCeosClick} style={{ cursor: "pointer" }}>
+        <Ceos />
+      </div>
       {data ? (
         <span className={logined_container}>
           <LoginedHeader name={data.result.name} />
