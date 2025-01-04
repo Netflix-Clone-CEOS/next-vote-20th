f3f3fe513a6e00d25fca23a536d5b424e708896f
Gabin Lee <gb94@ewhain.net>
2025-01-04 18:29:31 +0900

    [ feat ] 파트장 투표 페이지 돌아가기 버튼

--- vote/src/PartLead/components/results/Button.tsx
+++ vote/src/PartLead/components/results/Button.tsx
@@ -1,7 +1,24 @@
+"use client";
+
 import { resultBtn } from "@/DemoDay/styles/results/results.css";
 import { pretendardSemiBold } from "@/styles/font.css";
 import React from "react";
+import { useRouter } from "next/navigation";
 
 export default function Button() {
-  return <div className={`${resultBtn} ${pretendardSemiBold}`}>돌아가기</div>;
+  const router = useRouter();
+
+  const handleClick = () => {
+    router.push("/partleadvote");
+  };
+
+  return (
+    <div
+      className={`${resultBtn} ${pretendardSemiBold}`}
+      onClick={handleClick}
+      style={{ cursor: "pointer" }}
+    >
+      돌아가기
+    </div>
+  );
 }
